3643e3490b18d7726ee3c7304ba016a937f80ddc
tarasov_es <vosarat1995@gmail.com>
2018-12-14 17:57:45 +0300

    Добавлен проект для вывода на консоль генерируемого sql

---
+++ Artest/Artest.EFCore.SqlGenerationTests/PrintCreatedSql.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+
+using Artest.EFCoreSample;
+
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#pragma warning disable 618
+
+namespace Artest.EFCore.SqlGenerationTests
+{
+	[TestClass]
+	public class PrintCreatedSql
+	{
+		private LoggerFactory loggerFactory = new LoggerFactory(new[]
+		                                                        {
+				                                                        new ConsoleLoggerProvider((category, level) =>
+						                                                                                  category == DbLoggerCategory.Database.Command.Name
+						                                                                                  && level == LogLevel.Debug, true)
+		                                                        });
+
+		[TestMethod]
+		public void Case1()
+		{
+			var connection = new SqliteConnection("DataSource=:memory:");
+			connection.Open();
+
+			try
+			{
+				var options = new DbContextOptionsBuilder<BloggingContext>()
+				              .UseSqlite(connection)
+				              .Options;
+
+				using (var context = new BloggingContext(options))
+				{
+					context.Database.EnsureCreated();
+				}
+
+				options = new DbContextOptionsBuilder<BloggingContext>()
+				              .UseSqlite(connection)
+				              .UseLoggerFactory(this.loggerFactory)
+				              .Options;
+
+				using (var context = new BloggingContext(options))
+				{
+					var selectQuery = from b in context.Blogs
+					                  join p in context.Posts on b.Id equals p.Id into ps from p in ps.DefaultIfEmpty()
+					                  select new { b.Url, Titul = p.Title };
+
+
+					var posts = selectQuery.Where(d => d.Url == "test url" && d.Titul == "test title").ToList();
+					Assert.AreEqual(0, posts.Count);
+				}
+			}
+			finally
+			{
+				connection.Close();
+			}
+		}
+	}
+}
--- Artest/Artest.EFCoreSample/Models/Post.cs
+++ Artest/Artest.EFCoreSample/Models/Post.cs
@@ -9,6 +9,7 @@ namespace Artest.EFCoreSample.Models
 		public int Id { get; set; }
 		public string Title { get; set; }
 		public string Content { get; set; }
+		public int BlogId { get; set; }
 
 		public Blog Blog { get; set; }
 	}
